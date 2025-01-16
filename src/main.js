d9d2da4710dfce9633d00c29dcfe468f0389c375
Zeyu Li <go72wog@mytum.de>
2025-01-16 01:51:31 +0100

    remove js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.scss'
-import CrystalDashboard from './CrystalDashboard.vue'
-
-createApp(CrystalDashboard)
-    .use(createPinia())
-    .mount('#app')
